41343baad33bec53b7b997832a1ea3becfb91c0a
ExactHarmony917 <sammi9860arjun@gmail.com>
2025-04-15 15:42:24 +0530

    Added "Column" struct. Implemented resize function. Started main. !!Other files need refactoring.

--- src/cell_operations.rs
+++ src/cell_operations.rs
@@ -3,8 +3,10 @@
 use crate::ast::{Expr, Addr};
 use std::cell::RefCell;
 use std::collections::BTreeSet;
+use std::ops::{Index, IndexMut};
 #[allow(unused_imports)]
 use std::rc::{Rc, Weak};
+
 
 
 #[derive(Debug, Clone)]
@@ -57,7 +59,7 @@ impl std::fmt::Debug for CellFunc
         if let Some(dest_rc) = self.destination.upgrade() 
         {
             let dest = dest_rc.borrow();
-            writeln!(f, "Cell({}, {})", dest.row, dest.col)?;
+            writeln!(f, "Cell({}, {})", dest.addr.row, dest.addr.col)?;
         } 
         else 
         {
@@ -83,8 +85,6 @@ impl CellFunc
 #[derive(Debug, Clone)]
 pub struct Cell 
 {
-    pub row: u32,
-    pub col: u32,
     pub addr: Addr,
     pub value: ValueType,
     pub cell_func: Option<CellFunc>,
@@ -94,12 +94,10 @@ pub struct Cell
 
 impl Cell 
 {
-    pub fn new(row: u32, col: u32, addr: Addr) -> Self 
+    pub fn new(addr: Addr) -> Self 
     {
         Cell 
         {
-            row,
-            col,
             addr,
             value: ValueType::IntegerValue(0),
             cell_func: None,
@@ -110,9 +108,54 @@ impl Cell
 }
 
 #[derive(Debug, Clone)]
+pub struct Column
+{
+    cells: Vec<Rc<RefCell<Cell>>>,
+    sheet_number: u32,
+    col_number: u32
+}
+
+impl IndexMut<usize> for Column
+{
+
+    fn index_mut(&mut self, ind: usize) -> &mut Rc<RefCell<Cell>> { //NOTE: Rc ko as mut bhejna hota hai kya
+        if self.cells.len() <= ind
+        {
+            let mut p = self.cells.len() as u32;
+            self.cells.resize_with(ind+1, || {p += 1; Rc::new(RefCell::new(Cell::new(Addr{sheet: self.sheet_number, row: p, col: self.col_number})))});
+        }
+        return &mut self.cells[ind]   //NOTE: Ye mut, & mut waherah dekh lena theek se koi please. (┬┬﹏┬┬)
+    }   
+}
+
+impl Index<usize> for Column   //NOTE: This is not needed for my part, I guessed this would be needed in other part so left this here. 
+{
+    type Output = Rc<RefCell<Cell>>;
+
+    fn index(&self, ind: usize) -> &Self::Output {
+        &self.cells[ind]
+    }   
+}
+
+impl Column
+{
+    fn new(col_number: u32, sheet_number: u32) -> Self {
+        Column{cells: vec![], col_number, sheet_number}
+    }
+
+    fn truncate(&mut self, new_len: usize) {
+        self.cells.truncate(new_len);
+    }
+    fn len(&self) -> usize {  
+        self.cells.len()
+    }
+}
+
+
+#[derive(Debug, Clone)]
 pub struct Sheet
 {
-    pub data: Vec<RefCell<Vec<Rc<RefCell<Cell>>>>>,
+    pub data: Vec<RefCell<Column>>,
     pub rows: u32,
     pub columns: u32,
     pub sheet_idx: u32,
@@ -122,28 +165,47 @@ impl Sheet
 {
     pub fn new(sheet_idx: u32, sheet_name: String, col: u32) -> Self 
     {
-        Sheet 
+        let mut s = Sheet 
         {
-            data: vec![RefCell::new(vec![]); col as usize], 
-            rows: 0,
-            columns: col,
+            data: vec![], //NOTE: Empty vector new se banana chahiye?
+            rows: 0, //Number of rows in each column
+            columns: 0, //Number of columns
             sheet_idx,
             sheet_name
-        }
+        };
+        s.resize(0, col as usize);  //NOTE: Ye u32 and usize wali cheez sort kar lena please
+        s
     }
-    pub fn resize(&mut self, row_num: u32, col_num:u32)         /////////////////////////////////////////// PENDING
+    pub fn resize(&mut self, row_num: usize, col_num: usize)   
     {
-        if self.data.len() < row_num as usize
         {
-            for _ in self.data.len()..row_num as usize 
+            let mut p = self.columns as u32;  //Assuming self.columns == self.data.len() //NOTE: Hope to god that this does not cause issues??
+            self.data.resize_with(col_num, || { p += 1; RefCell::new(Column::new(p-1, self.sheet_idx))}); //NOTE!!! : Defaulting sheet number to 0 for now. Must be changed.
+        }
+        self.columns = col_num as u32;  //NOTE: self.rows and (neeche,) self.columns u32 hai to unko "as usize" use karna pada. Har jaga otherwise usize lagega. If possible, sheet struct mai usize kar dena inko.
+
+        if row_num < (self.rows as usize) {
+            for i in 0..col_num
             {
-                let temp: Vec<Rc<RefCell<Cell>>> = vec![];
-                self.data.push(RefCell::new(temp));
+                let mut col = self.data[i].borrow_mut();   
+                if col.len() > row_num {              //NOTE: This check is redundant, as truncate does not do anything if given length is greater than current length. Left this for readability. Needed???
+                    col.truncate(row_num) 
+                }
             }
-            if col_num < self.columns 
-            {
-                // to do by arjun 
-            }
-        } 
+        }
+        self.rows = row_num as u32;
+        // if self.data.len() < row_num as usize
+        // {
+        //     for _ in self.data.len()..row_num as usize 
+        //     {
+        //         let temp: Vec<Rc<RefCell<Cell>>> = vec![];
+        //         self.data.push(RefCell::new(temp));
+        //     }
+        //     if col_num < self.columns 
+        //     {
+        //         // to do by arjun 
+        //     }
+        // } 
+
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,55 @@
 pub mod ast;
 pub mod tokens;
-// pub mod cell_operations;
+pub mod cell_operations;
 // pub mod evaluate_operations;
 use lalrpop_util::lalrpop_mod;
 use logos::Logos;
+use crate::cell_operations::{Sheet};
+use std::io;
 
 lalrpop_mod!(pub grammar); // include the generated parser
 
+/* STUFF TO DO:
+O - initialise
+O - ask input
+O - If error, report respective error, restart
+O - If not error:
+    O - Check required cells:
+        O - If out of range, report error (In extension: Suggest resizing)
+        O - Initialise if not done yet
+    O - Give new function to - cell
+    O - Give old and new function to - evaluate()
+    O - If result is error (loop) then report error.
+restart
+*/
+
+struct SheetNames {
+    map: Vec<(String, u32)>
+}
+
+impl SheetNames {
+    numFromName
+}
+
+
+
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
+
+    let r: u64 = std::env::args().nth(1)
+        .expect("Row number not entered (First arg missing)")
+        .parse().expect("Invalid input for Row number (First arg)");
+    
+    let c: u64 = std::env::args().nth(2)
+        .expect("Column number not entered (Second arg missing)")
+        .parse().expect("Invalid input for Column number (Second arg)");
+
+//sheets: &Vec<Rc<RefCell<Sheet>>>
+
+    // let sheets: Vec<Rc<RefCell<Sheet>>> = Vec
+
+
+
     let file = std::env::args().nth(1).unwrap_or("test.txt".into());
     let source_code = std::fs::read_to_string(file)?;
     // let source_code = "11+4+56";
@@ -20,7 +61,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
     });
     
     let parser = grammar::CommandParser::new();
-    let ast = parser.parse(lexer)?;
+    let ast = parser.parse(5, lexer)?;
 
     println!("{:?}", ast);
     Ok(())
